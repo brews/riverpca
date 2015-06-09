19db417b8b530e9a12ae6a032c4d13af5a4a96ab
Brewster Malevich <sbmalev@gmail.com>
2015-06-09 05:18:13 -0700

    Function'ed all the python notebooks. Still need to do composites and sig tests.

--- tools.py
+++ tools.py
@@ -4,6 +4,7 @@
 # Random collections of often-used code for the project.
 
 import sqlite3
+import string
 from calendar import monthrange
 import numpy as np
 import pandas as pd
@@ -69,9 +70,31 @@ def varimax(Phi, gamma = 1.0, q = 20, tol = 1e-6):
             break
     return (np.dot(Phi, R), R)
 
+def plot_northtest(x, nmodes=10):
+    """Screeplot `nmodes` leading modes from EOFS solver instance `x`"""
+    fig = plt.figure(figsize = (3, 3))
+    frac_var = x.varianceFraction(nmodes)
+    err = x.northTest(nmodes, vfscaled = True)
+    plt.errorbar(np.arange(nmodes) + 1, frac_var, yerr = err, fmt = "o")
+    plt.xlim(0.5, nmodes + 0.5)
+    plt.xlabel("Component")
+    plt.ylabel("Fraction of variance")
+    return fig
+
+def plot_pc(x, yr, nmodes=10):
+    """Plot `nmodes` leading PCs from EOFS solver instance `x` over corresponding array of years, `yr`"""
+    pc = x.pcs(npcs = nmodes, pcscaling = 1)
+    frac_var = x.varianceFraction(nmodes)
+    fig, axes = plt.subplots(figsize = (9.5, 6.5), nrows = nmodes, ncols = 1, sharex = True, sharey = True)
+    for i in range(nmodes):
+        axes.flat[i].plot(yr, pc[:, i], "-o")
+        axes.flat[i].set_title("PC " + str(i + 1) + " (" + str(np.round(frac_var[i] * 100, 1)) + "%)")
+    return fig
+
 def plot_trendmap(x, lat, lon):
     """Point map colored to show the trend of western US gages"""
-    f = plt.figure(figsize = (4, 6))
+    fig = plt.figure(figsize = (4, 6))
+    ax = fig.add_axes([0.1,0.1,0.8,0.8])
     m = Basemap(width = 2000000, height = 2300000, 
                 resolution = 'l', projection = 'stere', 
                 lat_ts = 40.0, 
@@ -82,11 +105,34 @@ def plot_trendmap(x, lat, lon):
     m.scatter(lon, lat, c = x.tolist(), s = 30, latlon = True, edgecolors='k')
     m.colorbar()
     plt.title("Trend coef")
-    return f
+    return fig
+
+def plot_eof(x, lat, lon, nmodes=10):
+    """Plot covariance map for `nmodes` EOFS of EOFS solver instance `x`."""
+    eof = x.eofsAsCovariance(neofs = nmodes)
+    frac_var = x.varianceFraction(nmodes)
+    fig, axes = plt.subplots(figsize = (9.5, 6.5), nrows = nmodes, ncols = 1)
+    divs = np.linspace(np.floor(eof.min()), np.ceil(eof.max()), 21)
+    for i in range(nmodes):
+        m = Basemap(ax = axes.flat[i], width = 2000000, height = 2300000, 
+                    resolution = 'l', projection = 'stere', 
+                    lat_ts = 40.0, 
+                    lat_0 = 40.0, lon_0 = -114.0)
+        x, y = m(lon, lat)
+        m.drawcoastlines(linewidth = 0.7, color = "#696969")
+        m.drawstates(linewidth = 0.7, color = "#696969")
+        m.drawcountries(linewidth = 0.7, color = "#696969")
+        ctf1 = m.contourf(x, y, eof[i].squeeze(), divs, cmap = plt.cm.RdBu_r, tri = True)
+        cb = m.colorbar(ctf1)
+        cb.set_label("cov")
+        m.scatter(x, y, facecolors = "none", edgecolor = "k")
+        axes.flat[i].set_title("EOF " + str(i + 1) + " (" + str(np.round(frac_var[i] * 100, 1)) + "%)")
+    return fig
 
 def plot_gagesmap(lat, lon):
     """Create a simple sample map of USGS gages in the Western US, given lat/lon"""
-    f = plt.figure(figsize = (4, 6))
+    fig = plt.figure(figsize = (4, 6))
+    ax = fig.add_axes([0.1,0.1,0.8,0.8])
     m = Basemap(width = 2000000, height = 2300000, 
                 resolution = 'l', projection = 'stere', 
                 lat_ts = 40.0, 
@@ -102,7 +148,45 @@ def plot_gagesmap(lat, lon):
     m.drawmeridians(meridians, labels = [False, True, False, True], color = "#333333")
     m.scatter(lon, lat, s = 30, marker = 'o', latlon = True, facecolors = "none", edgecolors='r')
     plt.title("n = " + str(len(lon)))
-    return f
+    return fig
+
+def plot_pearson(x, field, lat, lon, nmodes=10, alpha=[0.05], msk = False, world_map = False):
+    """Plot pearson correlations of "nmodes."""
+    pc = x.pcs(npcs = nmodes, pcscaling = 1)
+    fig, axes = plt.subplots(figsize = (9.5, 6.5), nrows = nmodes, ncols = 1)
+    divs = np.linspace(-1, 1, 21)
+    for i in range(nmodes):
+        r, p = pearson_corr(pc[:, i], field.copy())
+        if np.any(msk):
+            r = np.ma.masked_array(r, msk)
+            p = np.ma.masked_array(p, msk)
+        m = None
+        if world_map:
+            m = Basemap(ax = axes.flat[i], projection = "robin", lon_0 = 180, resolution = "c")
+        else:
+            m = Basemap(ax = axes.flat[i], projection = 'npstere', boundinglat = 20, lon_0 = 210, resolution='c')
+        x, y = m(lon, lat)
+        m.drawcoastlines(color = "#696969")
+        m.drawparallels(np.arange(-90, 110, 20), color = "#696969")
+        m.drawmeridians(np.arange(0, 360, 60), color = "#696969")
+        ctf1 = m.contourf(x, y, r, divs, cmap = plt.cm.RdBu_r)
+        ctf2 = m.contour(x, y, p, alpha, colors = "k")
+        cb = m.colorbar(ctf1)
+        cb.set_label("r")
+        axes.flat[i].set_title("PC " + str(i + 1))
+    return fig
+
+def pearson_corr(x, field):
+    """Pearson correlation between a 1D time series `x` and field array `f` (with 3 dimensions, the first is time)"""
+    f_oldshape = field.shape
+    field.shape = (f_oldshape[0], f_oldshape[1] * f_oldshape[2])
+    r = np.empty((f_oldshape[1] * f_oldshape[2]))
+    p = np.empty(r.shape)
+    for i in range(field.shape[1]):
+        r[i], p[i] = stats.pearsonr(x, field[:, i])
+    r.shape = (f_oldshape[1], f_oldshape[2])
+    p.shape = r.shape
+    return r, p
 
 def ttest_proportion_sig(a, b, alpha):
     """Returns the proportion of significant Welch's t-test results"""
@@ -112,16 +196,17 @@ def ttest_proportion_sig(a, b, alpha):
     # return (p <= alpha).sum()/p.size, t, p
     pass
 
-def field_composite(series, yr, divisions=4):
+def field_composite(series, yr, field, divisions=2):
     """Return SST or Z field composites for extreme ends of a time series"""
-    quarts = pd.qcut(series, np.linspace(0, 1, divisions), labels = ["low", "mid", "high"])
-    composite = {"low": field[(quarts == "low")], "high": field[(quarts == "high")]}
-    years = {"low": yr[(quarts == "low")], "high": yr[(quarts == "high")]}
+    labs = list(string.ascii_lowercase)[:divisions]
+    quarts = pd.qcut(series, np.linspace(0, 1, divisions + 1), labels = labs)
+    composite = {"low": field[(quarts == labs[0])], "high": field[(quarts == labs[-1])]}
+    years = {"low": yr[(quarts == labs[0])], "high": yr[(quarts == labs[-1])]}
     return composite, years
 
 def composite_mc_ttest(series, yr, field, alpha=0.1, nruns=500, divisions=4):
     """Field significance for Welch's t-test of field composites"""
-    composite, years = field_composite(series, yr)
+    composite, years = field_composite(series, yr, field)
     proportion_sig, t, p = ttest_proportion_sig(composite["high"], composite["low"], alpha = alpha)
     low_bound = len(years["high"])
     high_bound = low_bound + len(years["low"])
@@ -133,9 +218,10 @@ def composite_mc_ttest(series, yr, field, alpha=0.1, nruns=500, divisions=4):
         mc_props[i] = ttest_proportion_sig(population_a, population_b, alpha)
     return p, stats.percentileofscore(mc_props, proportion_sig, kind = "rank")
 
-def plot_field_composite(series, field):
+def plot_field_composite(series, yr, field, divisions):
     """Compare SST or Z field composites for extreme terciles of a time series"""
-    pass
+    comp, yrs = field_composite(series, yr, field, divisions)
+    # wings = np.max([np.absolute([x.min() for x in ), np.absolute(composit_anoms.max())])
 #     # Composite map of NDJ mean 500 mb height anomalies.
 #     erai = np.load(ERAI_PATH)
 #     msk_time = (erai["time"] >= SHORTCALYEAR_LOW + 1) & (erai["time"] <= SHORTCALYEAR_HIGH)
