d1d9cfc68a5437d5db26331b6421b23c255177a2
Brewster Malevich <sbmalev@gmail.com>
2015-06-15 13:19:32 -0700

    Removed non-essential rivers from river db creation script and data dir.

--- create_riversdb.py
+++ create_riversdb.py
@@ -3,6 +3,9 @@
 # and metadata. We're also carefully cleaning the data here.
 
 import sqlite3 as sql
+import pandas as pd
+import numpy as np
+import tools
 
 DATACACHE = "./data/rivers/"
 DB_PATH = "./data/riversdb.sqlite"
@@ -77,45 +80,6 @@ def main():
            "ReconMeta": [],
            "Recon": []}
 
-    # Arkansas River at Canon City, CO
-    # From http://treeflow.info/ark/index.html on 2011-11-12
-    # Arkansas River
-    gage_name = "Arkansas River at Canon City, CO"
-    recon_name = "Reconstructed Arkansas River at Canon City, CO"
-    nan = "-9999"
-    skip_lines = 14
-    # The values for each of the columns in the raw file, starting from 0.
-    year_column = 0
-    gage_column = 2
-    recon_column = 1
-    conversion_multiple = 3.90875156e-5 # acre feet/yr to m^3/s
-    raw_lines = []
-    with open(DATACACHE + "arkansascanoncity.txt", "r") as fl:
-        raw_lines = fl.readlines() 
-
-    out["GageMeta"].append((gage_name,
-                         "Arkansas River basin",
-                         "Arkansas River",
-                         "m^3/s",
-                         38.433889,
-                         -105.256667,
-                         "",  # Citation
-                         "",  # Notes
-                         "http://treeflow.info/ark/arkansascanoncity.txt",  # URL
-                         "2011-11-12"))
-    out["ReconMeta"].append((recon_name,
-                             "m^3/s",
-                             gage_name,
-                             "",  # Citation
-                             "", # Notes
-                             "http://treeflow.info/ark/arkansascanoncity.txt",
-                             "2011-11-12"))
-    for line in raw_lines[skip_lines:]:
-        line = line.rstrip().split()
-        if line[gage_column] != nan:
-            out["Gage"].append((gage_name, int(line[year_column]), int(line[gage_column]) * conversion_multiple))
-        if line[recon_column] != nan:
-            out["Recon"].append((gage_name, int(line[year_column]), int(line[recon_column]) * conversion_multiple))
 
     # Colorado River at Lees Ferry, AZ
     # From http://treeflow.info/upco/index.html on 2011-11-12
@@ -157,81 +121,6 @@ def main():
         if line[recon_column] != nan:
             out["Recon"].append((gage_name, int(line[year_column]), int(line[recon_column]) * conversion_multiple))
 
-    # Missouri River. Recieved 2011-11-18 in personal communivation
-    # from Connie Woodhouse.
-    # NOTE: Want column 0 and a water-year sum of the other columns.
-    gage_name = "Missouri River at Toston, MT"
-    nan = None
-    skip_lines = 6
-    # The values for each of the columns in the raw file, starting from 0.
-    year_column = 0
-    gage_column = None
-    recon_column = None
-    conversion_multiple = 0.0390875156 # thousand acre feet/yr to m^3/s
-    raw_lines = []
-    with open(DATACACHE + "Natural_Flows_Missouri_Toston.csv", "r") as fl:
-        raw_lines = fl.readlines() 
-
-    out["GageMeta"].append((gage_name,
-                         "Upper Missouri River basin",
-                         "Missouri River",
-                         "m^3/s",
-                         46.146111,
-                         -111.419722,
-                         "From email personal communication with Connie Woodhouse.",  # Citation
-                         "Recieved 2011-11-18 in personal communivation from Connie Woodhouse.",  # Notes
-                         "",  # URL
-                         "2011-11-18"))
-    # This is a bit tricky because we have monthly values and need to get water-year (Oct-Sept) sums.
-    prev_cal_year = None
-    prev_cal_year_sum = None
-    for line in raw_lines[skip_lines:]:
-        line = line.rstrip().split(",")
-        cal_year = int(line[0])
-        if prev_cal_year == (cal_year - 1):
-            water_year_value = sum([float(i) for i in line[1:10]]) + prev_cal_year_sum
-            out["Gage"].append((gage_name, cal_year, water_year_value * conversion_multiple))
-        prev_cal_year_sum = sum([float(i) for i in line[10:13]])
-        prev_cal_year = cal_year
-
-    # Rio Grande at Otowi Bridge, NM
-    # From http://treeflow.info/riogr/riograndeotowinrcs.txt on 2012-09-04
-    gage_name = "Rio Grande at Otowi Bridge, NM (NRCS flows)"
-    recon_name = "Reconstructed Rio Grande at Otowi Bridge, NM (NRCS flows)"
-    nan = "-9999"
-    skip_lines = 14
-    # The values for each of the columns in the raw file, starting from 0.
-    year_column = 0
-    gage_column = 2
-    recon_column = 1
-    conversion_multiple = 3.90875156e-5 # acre feet/yr to m^3/s
-    raw_lines = []
-    with open(DATACACHE + "riograndeotowinrcs.txt", "r") as fl:
-        raw_lines = fl.readlines() 
-
-    out["GageMeta"].append((gage_name,
-                         "Rio Grande River basin",
-                         "Rio Grande River",
-                         "m^3/s",
-                         35.8745,
-                         -106.142444,
-                         "",  # Citation
-                         "",  # Notes
-                         "http://treeflow.info/riogr/riograndeotowinrcs.txt",  # URL
-                         "2012-09-04"))
-    out["ReconMeta"].append((recon_name,
-                             "m^3/s",
-                             gage_name,
-                             "",
-                             "", # Notes
-                             "http://treeflow.info/riogr/riograndeotowinrcs.txt",
-                             "2012-09-04"))
-    for line in raw_lines[skip_lines:]:
-        line = line.rstrip().split()
-        if line[gage_column] != nan:
-            out["Gage"].append((gage_name, int(line[year_column]), int(line[gage_column]) * conversion_multiple))
-        if line[recon_column] != nan:
-            out["Recon"].append((gage_name, int(line[year_column]), int(line[recon_column]) * conversion_multiple))
 
     # Sacramento Four Rivers Index, CA
     # From personal communication with Dave Meko on 2011-11-12.
@@ -265,44 +154,6 @@ def main():
         if line[gage_column] != nan:
             out["Gage"].append((gage_name, int(line[year_column]), float(line[gage_column]) * conversion_multiple))
 
-    # Salinas River at Paso Robles, CA
-    # From http://treeflow.info/cali/salinas.txt on 2011-11-12
-    gage_name = "Salinas River at Paso Robles, CA"
-    recon_name = "Reconstructed Salinas River at Paso Robles, CA"
-    nan = "-9999"
-    skip_lines = 18
-    # The values for each of the columns in the raw file, starting from 0.
-    year_column = 0
-    gage_column = 2
-    recon_column = 1
-    conversion_multiple = 0.0283168466 # ft^3/s to m^3/s
-    raw_lines = []
-    with open(DATACACHE + "salinas.txt", "r") as fl:
-        raw_lines = fl.readlines() 
-
-    out["GageMeta"].append((gage_name,
-                         "Salinas River Basin",
-                         "Salinas River",
-                         "m^3/s",
-                         35.628611,
-                         -120.683333,
-                         "",  # Citation
-                         "",  # Notes
-                         "http://treeflow.info/cali/salinas.txt",  # URL
-                         "2011-11-12"))
-    out["ReconMeta"].append((recon_name,
-                             "m^3/s",
-                             gage_name,
-                             "",
-                             "", # Notes
-                             "http://treeflow.info/cali/salinas.txt",
-                             "2011-11-12"))
-    for line in raw_lines[skip_lines:]:
-      line = line.rstrip().split()
-      if line[gage_column] != nan:
-          out["Gage"].append((gage_name, int(line[year_column]), float(line[gage_column]) * conversion_multiple))
-      if line[recon_column] != nan:
-          out["Recon"].append((gage_name, int(line[year_column]), float(line[recon_column]) * conversion_multiple))
 
     # "Jackson Lake at Dam on Snake River near Moran, WY"
     # From http://treeflow.info/pnw/index.html on 2011-11-12
@@ -343,6 +194,7 @@ def main():
         if line[recon_column] != nan:
             out["Recon"].append((gage_name, int(line[year_column]), int(line[recon_column]) * conversion_multiple))
 
+
     # South Platte River at South Platte, CO
     # From http://treeflow.info/platte/splattesplatte.txt on 2012-09-04
     gage_name = "South Platte River at South Platte, CO"
@@ -381,6 +233,7 @@ def main():
             out["Gage"].append((gage_name, int(line[year_column]), int(line[gage_column]) * conversion_multiple))
         if line[recon_column] != nan:
             out["Recon"].append((gage_name, int(line[year_column]), int(line[recon_column]) * conversion_multiple))
+
 
     # Salt-Verde-Tonto waterways, AZ
     # From http://treeflow.info/loco/index.html on 2011-11-12
@@ -421,152 +274,39 @@ def main():
         if line[recon_column] != nan:
             out["Recon"].append((gage_name, int(line[year_column]), int(line[recon_column]) * conversion_multiple))
 
-    # Green River near Green River, WY
-    # from http://treeflow.info/upco/greengreenrivwybarnett.txt on 2013-06-27
-    gage_name = "Green River near Green River, WY"
-    recon_name = "Reconstructed Green River near Green River, WY"
-    nan = "-9999"
-    skip_lines = 16
-    # The values for each of the columns in the raw file, starting from 0.
-    year_column = 0
-    gage_column = 2
-    recon_column = 1
-    conversion_multiple = 3.90875156e-5 # acre feet/yr to m^3/s
-    raw_lines = []
-    with open(DATACACHE + "greengreenrivwybarnett.txt", "r") as fl:
-        raw_lines = fl.readlines() 
 
-    out["GageMeta"].append((gage_name,
-                         "Upper Colorado River basin",
-                         "Green River",
-                         "m^3/s",
-                         41.516389,
-                         -109.448333,
-                         "",  # Citation
-                         "",  # Notes
-                         "http://treeflow.info/upco/greengreenrivwybarnett.html",  # URL
-                         "2014-01-31"))
-    out["ReconMeta"].append((recon_name,
-                             "m^3/s",
-                             gage_name,
-                             "",
-                             "", # Notes
-                             "http://treeflow.info/upco/greengreenrivwybarnett.html",
-                             "2014-01-31"))
-    for line in raw_lines[skip_lines:]:
-        line = line.rstrip().split()
-        if line[gage_column] != nan:
-            out["Gage"].append((gage_name, int(line[year_column]), int(line[gage_column]) * conversion_multiple))
-        if line[recon_column] != nan:
-            out["Recon"].append((gage_name, int(line[year_column]), int(line[recon_column]) * conversion_multiple))
-
-    # Green River near Greendale, UT
-    # from http://treeflow.info/upco/greengreendale.html
-    gage_name = "Green River near Greendale, UT"
-    recon_name = "Reconstructed Green River near Greendale, UT"
-    nan = "-9999"
-    skip_lines = 16
-    # The values for each of the columns in the raw file, starting from 0.
-    year_column = 0
-    gage_column = 2
-    recon_column = 1
-    conversion_multiple = 3.90875156e-5 # acre feet/yr to m^3/s
-    raw_lines = []
-    with open(DATACACHE + "greengreendale.txt", "r") as fl:
-        raw_lines = fl.readlines() 
-
-    out["GageMeta"].append((gage_name,
-                         "Upper Colorado River basin",
-                         "Green River",
-                         "m^3/s",
-                         40.908333,
-                         -109.422222,
-                         "",  # Citation
-                         "",  # Notes
-                         "http://treeflow.info/upco/greengreendale.html",  # URL
-                         "2014-01-31"))
-    out["ReconMeta"].append((recon_name,
-                             "m^3/s",
-                             gage_name,
-                             "",
-                             "", # Notes
-                             "http://treeflow.info/upco/greengreendale.html",
-                             "2014-01-31"))
-    for line in raw_lines[skip_lines:]:
-        line = line.rstrip().split()
-        if line[gage_column] != nan:
-            out["Gage"].append((gage_name, int(line[year_column]), int(line[gage_column]) * conversion_multiple))
-        if line[recon_column] != nan:
-            out["Recon"].append((gage_name, int(line[year_column]), int(line[recon_column]) * conversion_multiple))
-
-    # Cache la Poudre River at Canyon Mouth, CO
-    # from http://treeflow.info/platte/poudre.html
-    gage_name = "Cache la Poudre River at Canyon Mouth, CO"
-    recon_name = "Reconstructed Cache la Poudre River at Canyon Mouth, CO"
-    nan = "-9999"
-    skip_lines = 16
-    # The values for each of the columns in the raw file, starting from 0.
-    year_column = 0
-    gage_column = 2
-    recon_column = 1
-    conversion_multiple = 0.0390875156 # 1000 acre feet/yr to m^3/s
-    raw_lines = []
-    with open(DATACACHE + "poudre.txt", "r") as fl:
-        raw_lines = fl.readlines() 
-
-    out["GageMeta"].append((gage_name,
-                         "Platte River basin",
-                         "Cache la Poudre River",
-                         "m^3/s",
-                         40.664444,
-                         -105.223889,
-                         "",  # Citation
-                         "",  # Notes
-                         "http://treeflow.info/platte/poudre.html",  # URL
-                         "2014-01-31"))
-    out["ReconMeta"].append((recon_name,
-                             "m^3/s",
-                             gage_name,
-                             "",
-                             "", # Notes
-                             "http://treeflow.info/platte/poudre.html",
-                             "2014-01-31"))
-    for line in raw_lines[skip_lines:]:
-        line = line.rstrip().split()
-        if line[gage_column] != nan:
-            out["Gage"].append((gage_name, int(line[year_column]), int(line[gage_column]) * conversion_multiple))
-        if line[recon_column] != nan:
-            out["Recon"].append((gage_name, int(line[year_column]), int(line[recon_column]) * conversion_multiple))
-    
-    # Columbia River at Dalles. Recieved 2011-11-18 in personal communivation
-    # from Connie Woodhouse.
-    # TODO: Check the units on this. Assuming it's in ft^3/s and in wateryears?
+    # Columbia River. Recieved 2015-06-05 in personal communivation
+    # from Jeremy S. Littell, USGS Research Ecologist (jlittell@usgs.gov).
+    # Downloaded from http://warm.atmos.washington.edu/2860/products/sites/r7climate/subbasin_summaries/4030/nat_bias_adjusted_vic_streamflow_monthly_historical.dat on 2015-06-10.
+    # NOTE: Want column 0 and a water-year sum of the other columns.
     gage_name = "Columbia River at Dalles, OR"
-    recon_name = None
-    nan = None
-    skip_lines = 1
-    # The values for each of the columns in the raw file, starting from 0.
-    year_column = 0
-    gage_column = 1
-    recon_column = None
-    conversion_multiple = 0.0283168466 # ft^3/s to m^3/s
-    raw_lines = []
-    with open(DATACACHE + "ColumbiaFlow_Hamlet.csv", "r") as fl:
-      raw_lines = fl.readlines() 
 
     out["GageMeta"].append((gage_name,
                        "Columbia River basin",
                        "Columbia River",
                        "m^3/s",
-                       45.6075,
-                       -121.172222,
-                       "From email personal communication with Connie Woodhouse.",  # Citation
-                       "Recieved 2011-11-18 in personal communivation from Connie Woodhouse.",  # Notes
+                       45.605,
+                       -121.168,
+                       "From email personal communication with Jeremy S. Littell.",  # Citation
+                       "Downloaded 2015-06-10 from http://warm.atmos.washington.edu/2860/products/sites/r7climate/subbasin_summaries/4030/nat_bias_adjusted_vic_streamflow_monthly_historical.dat.",  # Notes
                        "",  # URL
-                       "2011-11-18"))
-    for line in raw_lines[skip_lines:]:
-      line = line.rstrip().split(",")
-      out["Gage"].append((gage_name, int(line[year_column]), float(line[gage_column]) * conversion_multiple))
+                       "2015-06-10"))   
+    # This is a bit tricky because we have monthly values and need to get water-year (Oct-Sept) sums.
+    d = pd.read_table(DATACACHE + "nat_bias_adjusted_vic_streamflow_monthly_historical.dat",
+                      delim_whitespace = True, skiprows = 1, 
+                      skip_blank_lines = True, header = None, 
+                      names = ("year", "month", "value"), 
+                      parse_dates = {"date": [0, 1]})
+    month_list = [x.month for x in d["date"]]
+    msk = np.array([x in (10, 11, 12) for x in month_list])
+    wy = np.array([x.year for x in d["date"]])
+    wy[msk] += 1
+    d["wateryear"] = wy
+    d = d.groupby("wateryear").aggregate(np.sum)
+    for r in d.to_records():
+        wateryear = int(r[0])
+        value = float(r[1])
+        out["Gage"].append((gage_name, wateryear, value))
 
     # "Trinity River at Lewiston, CA"
     # From http://cdec.water.ca.gov/cgi-progs/queryCSV?station_id=TNL&dur_code=M&sensor_num=65&start_date=5/25/1911&end_date=Now on 2014-02-12
