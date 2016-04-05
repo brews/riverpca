2955d12d9bcbd24bc00fd1b8c6f7a81b22d96bf2
Brewster Malevich <sbmalev@gmail.com>
2016-04-05 14:59:31 -0700

    Cleanup main script.

--- main.sh
+++ main.sh
@@ -4,14 +4,14 @@
 # Note there is a setup python script files in ./data/ERA-I/. This downloads big data so don't use it unless you have to.
 
 ./create_usgsdb.py
-./create_riversdb.py
+#./create_riversdb.py
 
 ./parse_20threanv2.py
 ./parse_erai.py
 ./parse_ersstv3.py
 ./parse_noaaoiv2.py
 
-./plot_samplemap_riversdb.py
-./plot_samplemap_usgs_short.py
-./plot_samplemap_usgs_long.py
-./plot_samplemap_usgs_sampledecay.py
+#./plot_samplemap_riversdb.py
+#./plot_samplemap_usgs_short.py
+#./plot_samplemap_usgs_long.py
+#./plot_samplemap_usgs_sampledecay.py
