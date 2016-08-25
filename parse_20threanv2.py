545ede1f1f6a44ed6143d7abe9abff09f88d3f3d
Brewster Malevich <sbmalev@gmail.com>
2016-08-25 11:21:03 -0700

    Playing with new xarray-like structure for parsed reanalysis data and play with new maps using cartopy in 1983- PCA

--- parse_20threanv2.py
+++ parse_20threanv2.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-# 2015-01-15
+# 2016-06-02
 # Parse 20th century reanalysis version 2 netCDF file `TWENTY_PATH` into 
 # winter and spring seasonal averages for pressure heights `LEVEL_TARGETS`. 
 # A numpy array is sent to `OUT_FILE`. It's structure is # Dim 1 is [NDJ, FMA],
@@ -11,47 +11,65 @@
 import datetime
 import numpy as np
 from netCDF4 import Dataset
+import pandas as pd
+import xarray as xr
 
-TWENTY_PATH = "./data/20th_rean_V2/hgt.mon.mean.nc"
+TWENTY_PATH = './data/20th_rean_V2/hgt.mon.mean.nc'
 ORIGIN_TIME = datetime.datetime(1800, 1, 1, 0, 0, 0)
 LEVEL_TARGETS = [700, 500, 250]
-OUT_FILE = "./data/20th_rean_V2.npz"
+OUT_FILE = './data/20th_rean_V2.npz'
+OUT_NC = './data/tcrv2_z500_season.nc'
 
-raw = Dataset(TWENTY_PATH)
-dates = [ORIGIN_TIME + datetime.timedelta(hours = x) for x in raw.variables["time"][:]]
-year_min = min([i.year for i in dates])
-year_max = max([i.year for i in dates])
-year_range = np.arange(year_min + 1, year_max)
-lon, lat = np.meshgrid(raw.variables["lon"][:], raw.variables["lat"][:])
-time = []
-# Dim 1 is [NDJ, FMA], dim 2 is levels [700, 500, 250], dim 3 is year, dim 4 is lat, dim 5 is lon:
-out = np.empty((4, len(LEVEL_TARGETS), len(year_range), raw.variables["hgt"].shape[-2], raw.variables["hgt"].shape[-1]))
-for j in range(len(year_range)):
-    yr = year_range[j]
-    msk_djf = []
-    msk_mam = []
-    msk_son = [] # Antecedent fall
-    msk_jja = [] # Antecedent summer
-    time.append(yr)
-    for d in range(len(dates)):
-        # If DJF
-        if (dates[d] == datetime.datetime(yr - 1, 12, 1, 0, 0)) or (dates[d] == datetime.datetime(yr, 1, 1, 0, 0)) or (dates[d] == datetime.datetime(yr, 2, 1, 0, 0)):
-            msk_djf.append(d)
-        # If MAM
-        if (dates[d] == datetime.datetime(yr, 3, 1, 0, 0)) or (dates[d] == datetime.datetime(yr, 4, 1, 0, 0)) or (dates[d] == datetime.datetime(yr, 5, 1, 0, 0)):
-            msk_mam.append(d)
-        # If -SON
-        if (dates[d] == datetime.datetime(yr - 1, 9, 1, 0, 0)) or (dates[d] == datetime.datetime(yr - 1, 10, 1, 0, 0)) or (dates[d] == datetime.datetime(yr - 1, 11, 1, 0, 0)):
-            msk_son.append(d)
-        # If -JJA
-        if (dates[d] == datetime.datetime(yr - 1, 6, 1, 0, 0)) or (dates[d] == datetime.datetime(yr - 1, 7, 1, 0, 0)) or (dates[d] == datetime.datetime(yr - 1, 8, 1, 0, 0)):
-            msk_jja.append(d)
 
-    for i in range(len(LEVEL_TARGETS)):
-        msk_levels = raw.variables["level"][:] == LEVEL_TARGETS[i]
-        out[0, i, j, :, :] = np.mean(raw.variables["hgt"][msk_djf, msk_levels], 0)
-        out[1, i, j, :, :] = np.mean(raw.variables["hgt"][msk_mam, msk_levels], 0)
-        out[2, i, j, :, :] = np.mean(raw.variables["hgt"][msk_son, msk_levels], 0)
-        out[3, i, j, :, :] = np.mean(raw.variables["hgt"][msk_jja, msk_levels], 0)
+def main():
+    raw = Dataset(TWENTY_PATH)
+    dates = [ORIGIN_TIME + datetime.timedelta(hours = x) for x in raw.variables["time"][:]]
+    dates.sort()
+    year_min = min([i.year for i in dates])
+    year_max = max([i.year for i in dates])
+    year_range = np.arange(year_min + 1, year_max)
+    lon, lat = np.meshgrid(raw.variables["lon"][:], raw.variables["lat"][:])
+    time = []
+    # Dim 1 is [NDJ, FMA], dim 2 is levels [700, 500, 250], dim 3 is year, dim 4 is lat, dim 5 is lon:
+    out = np.empty((4, len(LEVEL_TARGETS), len(year_range), raw.variables["hgt"].shape[-2], raw.variables["hgt"].shape[-1]))
+    for j in range(len(year_range)):
+        yr = year_range[j]
+        msk_djf = []
+        msk_mam = []
+        msk_son = [] # Antecedent fall
+        msk_jja = [] # Antecedent summer
+        time.append(yr)
+        for d in range(len(dates)):
+            # If DJF
+            if (dates[d] == datetime.datetime(yr - 1, 12, 1, 0, 0)) or (dates[d] == datetime.datetime(yr, 1, 1, 0, 0)) or (dates[d] == datetime.datetime(yr, 2, 1, 0, 0)):
+                msk_djf.append(d)
+            # If MAM
+            if (dates[d] == datetime.datetime(yr, 3, 1, 0, 0)) or (dates[d] == datetime.datetime(yr, 4, 1, 0, 0)) or (dates[d] == datetime.datetime(yr, 5, 1, 0, 0)):
+                msk_mam.append(d)
+            # If -SON
+            if (dates[d] == datetime.datetime(yr - 1, 9, 1, 0, 0)) or (dates[d] == datetime.datetime(yr - 1, 10, 1, 0, 0)) or (dates[d] == datetime.datetime(yr - 1, 11, 1, 0, 0)):
+                msk_son.append(d)
+            # If -JJA
+            if (dates[d] == datetime.datetime(yr - 1, 6, 1, 0, 0)) or (dates[d] == datetime.datetime(yr - 1, 7, 1, 0, 0)) or (dates[d] == datetime.datetime(yr - 1, 8, 1, 0, 0)):
+                msk_jja.append(d)
 
-np.savez_compressed(OUT_FILE, data = out, lat = lat, lon = lon, time = time)
+        for i in range(len(LEVEL_TARGETS)):
+            msk_levels = raw.variables["level"][:] == LEVEL_TARGETS[i]
+            out[0, i, j, :, :] = np.mean(raw.variables["hgt"][msk_djf, msk_levels], 0)
+            out[1, i, j, :, :] = np.mean(raw.variables["hgt"][msk_mam, msk_levels], 0)
+            out[2, i, j, :, :] = np.mean(raw.variables["hgt"][msk_son, msk_levels], 0)
+            out[3, i, j, :, :] = np.mean(raw.variables["hgt"][msk_jja, msk_levels], 0)
+
+    # np.savez_compressed(OUT_FILE, data = out, lat = lat, lon = lon, time = time)
+    ds = xr.Dataset({'z500': (['season', 'wy', 'lat', 'lon'], out[:, 1, :, :])},
+                    coords = {'lon': (['lon'], raw.variables['lon'][:]),
+                              'lat': (['lat'], raw.variables['lat'][:]),
+                              'wy': pd.date_range(str(year_range[0])+'-01-01', str(year_range[-1])+'-01-01', freq = 'AS'),
+                              'season': ['DJF', 'MAM', 'SON-1', 'JJA-1']})
+    ds.attrs['units'] = raw.variables["hgt"].units
+
+    ds.to_netcdf(OUT_NC, format = 'NETCDF4', engine = 'netcdf4', 
+        mode = 'w', encoding = {'z500': {'zlib': True}})
+
+if __name__ == '__main__':
+    main()
--- parse_ersstv3.py
+++ parse_ersstv3.py
@@ -1,55 +1,68 @@
 #! /usr/bin/env python3
-# 2015-01-21
+# 2016-06-03
 
 # Grab and parse the ERSST V3b dataset.
 
 import datetime
 import numpy as np
 from netCDF4 import Dataset
+import pandas as pd
+import xarray as xr
 
 ERSST_PATH = "./data/ERSST_V3b/sst.mnmean.nc"
 OUT_FILE = "./data/ersst.npz"
 ORIGIN_TIME = datetime.datetime(1800, 1, 1, 0, 0, 0)
+OUT_NC = './data/ersstv3b_season.nc'
 
-raw = Dataset(ERSST_PATH)
-dates = [ORIGIN_TIME + datetime.timedelta(days = x) for x in raw.variables["time"][:]]
-lat = raw.variables["lat"]
-lon = raw.variables["lon"]
-lon, lat = np.meshgrid(lon[:], lat[:])
-sst = raw.variables["sst"]
-time = []
+def main():
+    raw = Dataset(ERSST_PATH)
+    dates = [ORIGIN_TIME + datetime.timedelta(days = x) for x in raw.variables["time"][:]]
+    lat = raw.variables["lat"]
+    lon = raw.variables["lon"]
+    lon, lat = np.meshgrid(lon[:], lat[:])
+    sst = raw.variables["sst"]
+    time = []
 
-# year_min = min([i.year for i in dates])
-year_min = min([i.year for i in dates])
-year_max = max([i.year for i in dates])
-year_range = np.arange(year_min + 1, year_max)
+    # year_min = min([i.year for i in dates])
+    year_min = min([i.year for i in dates])
+    year_max = max([i.year for i in dates])
+    year_range = np.arange(year_min + 1, year_max)
 
-out = np.empty((4, len(year_range), raw.variables["sst"].shape[-2], raw.variables["sst"].shape[-1]))
-for j in range(len(year_range)):
-    yr = year_range[j]
-    time.append(yr)
-    msk_djf = []
-    msk_mam = []
-    msk_son = [] # Antecedent fall
-    msk_jja = [] # Antecedent summer
-    for d in range(len(dates)):
-        if (dates[d] == datetime.datetime(yr - 1, 12, 1, 0, 0)) or (dates[d] == datetime.datetime(yr, 1, 1, 0, 0)) or (dates[d] == datetime.datetime(yr, 2, 1, 0, 0)):
-            msk_djf.append(d)
-        if (dates[d] == datetime.datetime(yr, 3, 1, 0, 0)) or (dates[d] == datetime.datetime(yr, 4, 1, 0, 0)) or (dates[d] == datetime.datetime(yr, 5, 1, 0, 0)):
-            msk_mam.append(d)
-        if (dates[d] == datetime.datetime(yr - 1, 9, 1, 0, 0)) or (dates[d] == datetime.datetime(yr - 1, 10, 1, 0, 0)) or (dates[d] == datetime.datetime(yr - 1, 11, 1, 0, 0)):
-            msk_son.append(d)
-        if (dates[d] == datetime.datetime(yr - 1, 6, 1, 0, 0)) or (dates[d] == datetime.datetime(yr - 1, 7, 1, 0, 0)) or (dates[d] == datetime.datetime(yr - 1, 8, 1, 0, 0)):
-            msk_jja.append(d)
-    out[0, j, :, :] = np.mean(raw.variables["sst"][msk_djf], 0)
-    out[1, j, :, :] = np.mean(raw.variables["sst"][msk_mam], 0)
-    out[2, j, :, :] = np.mean(raw.variables["sst"][msk_son], 0)
-    out[3, j, :, :] = np.mean(raw.variables["sst"][msk_jja], 0)
+    out = np.empty((4, len(year_range), raw.variables["sst"].shape[-2], raw.variables["sst"].shape[-1]))
+    for j in range(len(year_range)):
+        yr = year_range[j]
+        time.append(yr)
+        msk_djf = []
+        msk_mam = []
+        msk_son = [] # Antecedent fall
+        msk_jja = [] # Antecedent summer
+        for d in range(len(dates)):
+            if (dates[d] == datetime.datetime(yr - 1, 12, 1, 0, 0)) or (dates[d] == datetime.datetime(yr, 1, 1, 0, 0)) or (dates[d] == datetime.datetime(yr, 2, 1, 0, 0)):
+                msk_djf.append(d)
+            if (dates[d] == datetime.datetime(yr, 3, 1, 0, 0)) or (dates[d] == datetime.datetime(yr, 4, 1, 0, 0)) or (dates[d] == datetime.datetime(yr, 5, 1, 0, 0)):
+                msk_mam.append(d)
+            if (dates[d] == datetime.datetime(yr - 1, 9, 1, 0, 0)) or (dates[d] == datetime.datetime(yr - 1, 10, 1, 0, 0)) or (dates[d] == datetime.datetime(yr - 1, 11, 1, 0, 0)):
+                msk_son.append(d)
+            if (dates[d] == datetime.datetime(yr - 1, 6, 1, 0, 0)) or (dates[d] == datetime.datetime(yr - 1, 7, 1, 0, 0)) or (dates[d] == datetime.datetime(yr - 1, 8, 1, 0, 0)):
+                msk_jja.append(d)
+        out[0, j, :, :] = np.mean(raw.variables["sst"][msk_djf], 0)
+        out[1, j, :, :] = np.mean(raw.variables["sst"][msk_mam], 0)
+        out[2, j, :, :] = np.mean(raw.variables["sst"][msk_son], 0)
+        out[3, j, :, :] = np.mean(raw.variables["sst"][msk_jja], 0)
 
 
-landmask = out[0, 0] == 0
-np.savez_compressed(OUT_FILE, data = out, lat = lat, lon = lon, time = time, landmask = landmask)
+    landmask = out[0, 0] == 0
+    # np.savez_compressed(OUT_FILE, data = out, lat = lat, lon = lon, time = time, landmask = landmask)
+    out_msk = np.where(~landmask[np.newaxis, np.newaxis, :, :], out, np.nan)
+    ds = xr.Dataset({'sst': (['season', 'wy', 'lat', 'lon'], out_msk)},
+                    coords = {'lon': (['lon'], raw.variables['lon'][:]),
+                              'lat': (['lat'], raw.variables['lat'][:]),
+                              'wy': pd.date_range(str(year_range[0])+'-01-01', str(year_range[-1])+'-01-01', freq = 'AS'),
+                              'season': ['DJF', 'MAM', 'SON-1', 'JJA-1']})
+    ds.attrs['units'] = raw.variables["sst"].units
 
+    ds.to_netcdf(OUT_NC, format = 'NETCDF4', engine = 'netcdf4', 
+        mode = 'w', encoding = {'sst': {'zlib': True}})
 
 # Plot on map for sanity checks.
 # plt.figure()
@@ -64,3 +77,6 @@ np.savez_compressed(OUT_FILE, data = out, lat = lat, lon = lon, time = time, lan
 # cb.set_label("SST (C)")
 # plt.title("NDJ ERSST V3b")
 # plt.show()
+
+if __name__ == '__main__':
+    main()
