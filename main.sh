0012994c3dfc179e23bb97bf2d8753617d752c85
Brewster Malevich <sbmalev@gmail.com>
2016-10-13 22:17:29 -0700

    Cleanup cruft scripts and update main.sh

--- main.sh
+++ main.sh
@@ -1,17 +1,7 @@
 #! /usr/bin/env bash
-# 2014-02-27
-
-# Note there is a setup python script files in ./data/ERA-I/. This downloads big data so don't use it unless you have to.
+# 2016-10-13
 
 ./create_usgsdb.py
-#./create_riversdb.py
 
 ./parse_20threanv2.py
-./parse_erai.py
 ./parse_ersstv3.py
-./parse_noaaoiv2.py
-
-#./plot_samplemap_riversdb.py
-#./plot_samplemap_usgs_short.py
-#./plot_samplemap_usgs_long.py
-#./plot_samplemap_usgs_sampledecay.py
